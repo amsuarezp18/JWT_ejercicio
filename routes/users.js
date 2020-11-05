4e498e64a18bf524d114fb11562ff5862ca6153e
amsuarezp <amsuarezp@gmail.com>
2020-11-05 18:22:31 -0500

    user.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var [createUser, login] = require('../controllers/user');
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+*/
+
+/* CREATE user */
+router.post('/', async function(req, res, next) {
+  
+});
+
+router.post('/login', async function(req, res, next) {
+  try {
+    const authUser = await login(req.body);
+    res.send(authUser);
+  } catch (error) {
+    res.send(403).json({
+      success: false,
+      message: 'Incorrect username or password'
+    })
+  }
 });
 
 module.exports = router;
